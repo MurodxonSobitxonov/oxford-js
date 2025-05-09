5c0805975637050d55c94796db7abbefb8d264c0
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-05-09 17:12:13 +0500

    changet

---
+++ Homework/4-dars/inde.js
@@ -0,0 +1,12 @@
+let a = +prompt('a?', '');
+
+switch (a) { 
+    case (a == 0) :
+    console.log(0);
+    break;
+    case (a == 1) :
+    console.log(1);
+    break;
+    case (a == 2 || a == 3) :
+        console.log('2,3');
+}
