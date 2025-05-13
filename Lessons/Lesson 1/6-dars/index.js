2faa624d025a8b8c656549bc7aca4334aa192957
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-05-13 15:55:49 +0500

    changet

---
+++ Lessons/Lesson 1/6-dars/index.js
@@ -0,0 +1,18 @@
+// for(let a = 0; a <= 10; a++) {
+//     if(a == 7) {
+//         console.log(a)
+//         break;
+//     }
+//     console.log(a)
+// }
+// for (let i = 1; i <= 10; i++) {
+//     if (!(i % 2)) {
+//         console.log(i);
+//     }
+// }
+for (let i = 1; i < 10; i++){
+    for (let j =1; j < 10; j++){
+        console.log(`${i} x ${j} = ${i * j}`);
+        
+    }
+}
