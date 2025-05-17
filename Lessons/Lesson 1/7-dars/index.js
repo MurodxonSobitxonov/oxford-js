efe56a45d197148ad4b53342b887f6d6093732b1
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-05-17 12:05:25 +0500

    changet

---
+++ Lessons/Lesson 1/7-dars/index.js
@@ -0,0 +1,31 @@
+// console.time()
+// let j = 1;
+// while(j <= 10){
+//     if(j % 2== 0){
+//         console.log(j , "juft");        
+//     }
+//     else {
+//         console.log(j, "toq");        
+//     }
+//     j++;
+
+// }
+// console.timeEnd()
+
+// console.time()
+// let k = 1;
+// do{
+//     if(k % 2 ){
+//         console.log(k, "toq");
+//     }
+//     else {
+//         console.log(k, "juft");
+//     }
+//     k++;
+// } while (k <= 10)
+//     console.timeEnd()
+let i = 1
+while (true){
+    console.log(i);
+    i++;
+}
