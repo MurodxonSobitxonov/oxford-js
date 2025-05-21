27d465ce4f9fdc0909e0d84c335affd8f818b018
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-05-21 20:19:17 +0500

    changed

--- Homework/8-dars/index.js
+++ Homework/8-dars/index.js
@@ -1,5 +1,5 @@
 // function ortachaArifmetik(sonlar) {
-//     if (sonlar.length === 0) return 0; 
+//     if (sonlar.length === 0) return 0;
 //     let summa = 0;
 
 //     for (let i = 0; i < sonlar.length; i++) {
@@ -16,3 +16,20 @@
 // console.log("O'rtacha arifmetik:", natija);
 // javob 6 chadi
 //--------------------------------------------------------------------------------------------
+function ortachaGeometrik(sonlar) {
+    if (sonlar.length === 0) return 0;
+
+    let kopaytma = 1;
+    for (let i = 0; i < sonlar.length; i++) {
+        kopaytma *= sonlar[i];
+    }
+
+    return Math.pow(kopaytma, 1 / sonlar.length);
+}
+
+// Misol:
+let massiv = [4, 7, 9];
+let natija = ortachaGeometrik(massiv);
+
+console.log("O'rtacha geometrik:", natija);
+  
