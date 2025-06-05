c4e2d0cdf170db120e22956e9dc5e4341b37ece3
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-06-05 14:26:02 +0500

    changed

---
+++ Lessons/Lesson 2/1-dars/index.js
@@ -0,0 +1,7 @@
+// let tugilganYil = prompt("Tug'ilgan yilingizni kiriting:");
+// let joriyYil = 2025;
+// let yosh = joriyYil - tugilganYil;
+
+// console.log(yosh + " yoshdasiz");
+
+// ___________________________________
