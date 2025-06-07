6f4aeed28763596c4a41bf1a3ccf6ec25356cb08
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-06-07 16:16:15 +0500

    changet

---
+++ Homework/4,-dars/index.js
@@ -0,0 +1,69 @@
+//4.1
+// let son = 5;
+// if (son > 0) {
+//     son += 1;
+// }
+// console.log("Natija:", son);
+//4.2
+// let son = 5;
+// if (son > 0) {
+//     son += 1;
+// } else {
+//     son -= 2;
+// }
+// console.log("Natija:", son);
+//4.3
+// let son = -3;
+// if (son > 0) {
+//     son += 1;
+// } else if (son < 0) {
+//     son -= 2;
+// } else {
+//     son = 10;
+// }
+// console.log("Natija:", son);
+//4.4
+// let a = 5, b = -3, c = 7;
+// let musbatlar = 0;
+// if (a > 0) musbatlar++;
+// if (b > 0) musbatlar++;
+// if (c > 0) musbatlar++;
+// console.log("Musbat sonlar soni:", musbatlar);
+//4.5
+// let a = 5, b = -3, c = 0;
+// let musbat = 0, manfiy = 0;
+// if (a > 0) musbat++;
+// else if (a < 0) manfiy++;
+
+// if (b > 0) musbat++;
+// else if (b < 0) manfiy++;
+
+// if (c > 0) musbat++;
+// else if (c < 0) manfiy++;
+
+// console.log("Musbat sonlar:", musbat);
+// console.log("Manfiy sonlar:", manfiy);
+//4.6
+// let a = 10, b = 15;
+// let katta = a > b ? a : b;
+// console.log("Kattasi:", katta);
+//4.7
+// let a = 10, b = 15;
+// let kichik = a < b ? 1 : 2;
+// console.log("Kichik son tartib raqami:", kichik);
+//4.8
+// let a = 10, b = 15;
+// if (a > b) {
+//     console.log("Birinchi:", a, "Ikkinchi:", b);
+// } else {
+//     console.log("Birinchi:", b, "Ikkinchi:", a);
+// }
+//4.9
+// let A = 20, B = 10;
+// if (A > B) {
+//     let temp = A;
+//     A = B;
+//     B = temp;
+// }
+// console.log("A:", A, "B:", B);
+//4.10
