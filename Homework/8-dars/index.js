d82741f01aada4162908ef539f4bbbecdf9cc4d7
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-06-24 15:07:00 +0500

    changed

--- Homework/8-dars/index.js
+++ Homework/8-dars/index.js
@@ -1,34 +1,16 @@
-// function ortachaArifmetik(sonlar) {
-//     if (sonlar.length === 0) return 0;
-//     let summa = 0;
+// let student = {
+//     firstName: "Eshmat",
+//     lastName: "Toshmatov",
+//     age: 20,
+//     course: "Frontend"
+// };
+// console.log(student.firstName, student.lastName, student.age,);
+let data = {
+    name: "Phone",
+    model: "iPhone",
+    price: 700,
+    quantity: 8
+};
 
-//     for (let i = 0; i < sonlar.length; i++) {
-//         summa += sonlar[i];
-//     }
-
-//     let ortacha = summa / sonlar.length;
-//     return ortacha;
-// }
-
-// // Misol uchun:
-// let qiymatlar = [4, 7, 6, 3, 10];
-// let natija = ortachaArifmetik(qiymatlar);
-// console.log("O'rtacha arifmetik:", natija);
-// javob 6 chadi
-//--------------------------------------------------------------------------------------------
-function ortachaGeometrik(sonlar) {
-    if (sonlar.length === 0) return 0;
-
-    let kopaytma = 1;
-    for (let i = 0; i < sonlar.length; i++) {
-        kopaytma *= sonlar[i];
-    }
-
-    return Math.pow(kopaytma, 1 / sonlar.length);
-}
-
-// Misol:
-let massiv = [4, 7, 9];
-let natija = ortachaGeometrik(massiv);
-
-console.log("O'rtacha geometrik:", natija);
+console.log("Jami qiymat:", data.price * data.quantity);
+  
---
+++ Lessons/Lesson 2/1-dars/8-dars/index.js
@@ -0,0 +1,12 @@
+let i = 3, a = 9, c = 27, b = 81;
+// function getArifmetika(arf1, arf2, arf3, arf4) {
+//     let arifmetika = (arf1 + arf2 + arf3 + arf4) / 4;
+//     console.log("O'rtacha arifmetik:", arifmetika);   
+// }
+// getArifmetika(i, a, c, b);
+
+function getGeometrik(geo1, geo2, geo3, geo4) {
+    let geometrik = (geo1 * geo2 * geo3 * geo4) ** (1 / 4);
+    console.log("O'rtacha geometrik:", geometrik);
+}
+getGeometrik(i, a, c, b);
