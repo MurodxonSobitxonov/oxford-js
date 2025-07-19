c12d25d3f6c808ed81ab6472377b2153cb2daa28
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-07-19 15:49:04 +0500

    changet

---
+++ Lessons/Lesson 2/1-dars/17-dars/index.js
@@ -0,0 +1,26 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < 10; i++){
+    // if (i % 2 !== 0) {
+    //     console.log(i);
+    // }
+// }
+   
+//--------------------------------------------
+
+// const arr = [1, 2, 3, 4, 5]
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+// console.log(sum);
+
+const arr = [14, -8, 3, -7, 9, -1]
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+        console.log(arr[i]);
+        
+    }
+}
+
+// reduce => reduce arreydagi qiymatlarini yig'indisini chiqaradi
