8d46a4b58ce081f0c4091b7bb2e772382c5ccdb3
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-08-25 14:39:51 +0500

    changet

--- Lessons/Lesson 2/24-dars/index.js
+++ Lessons/Lesson 2/24-dars/index.js
@@ -14,15 +14,17 @@
 // console.log(result);
 
 
-let arr = [1, 2, 3, 4];
+// let arr = [1, 2, 3, 4];
 
-let result = arr.reduce((a, b) => {
-  if (b % 2 === 0) {
-    a.juft.push(b + (b === 2 ? 0 : 2)); 
-  } else {
-    a.toq.push(b === 3 ? 4 : b); 
-  }
-  return a;
-}, { juft: [], toq: [] });
+// let result = arr.reduce((a, b) => {
+//   if (b % 2 === 0) {
+//     a.juft.push(b + (b === 2 ? 0 : 2));
+//   } else {
+//     a.toq.push(b === 3 ? 4 : b);
+//   }
+//   return a;
+// }, { juft: [], toq: [] });
 
-console.log(result);
+// console.log(result);
+
+// console.log(0b1011000);
