d1eb1ebc5ebd03d7c41f86df12622084daf29094
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-08-26 21:35:31 +0500

    changet

---
+++ Lessons/Lesson 2/26-dars/index.js
@@ -0,0 +1,12 @@
+function user({ name, age: yosh }) {
+  console.log(name, yosh);
+  
+}
+user({ name: "John", age: 20 });
+
+function sum({ ...arr }) {
+  console.log(arr);
+  
+}
+
+sum(1, 2, 3, 4, 5,)
